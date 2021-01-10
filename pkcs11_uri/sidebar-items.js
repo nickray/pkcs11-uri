45e6636f53d7fc0a82c59967edb93c72d084f871
nickray <nickray@users.noreply.github.com>
2021-01-10 21:19:15 +0000

    deploy: f79c390647ddebb22ba40d482d8797b925edfe22

--- pkcs11_uri/sidebar-items.js
+++ pkcs11_uri/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["ObjectClass",""]],"struct":[["PathAttributes",""],["Pkcs11Uri","Parsed RFC 7512 PKCS #11 URI"],["QueryAttributes",""],["Version",""]],"type":[["Context",""],["ObjectHandle",""],["SessionHandle",""],["SlotId",""]]});
+initSidebarItems({"enum":[["ObjectClass",""]],"fn":[["split_once",""]],"struct":[["PathAttributes",""],["Pkcs11Uri","Parsed RFC 7512 PKCS #11 URI"],["QueryAttributes",""],["Version",""]],"type":[["Context",""],["ObjectHandle",""],["SessionHandle",""],["SlotId",""]]});
